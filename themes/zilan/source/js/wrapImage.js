40e5b397a9d9cf5eb657c55867cd8999da3874c2
CatherineLiyuankun <muzilan1@qq.com>
2018-08-31 15:39:07 +0800

    update donate widget with fancybox

---
+++ themes/zilan/source/js/wrapImage.js
@@ -0,0 +1,20 @@
+  // Caption
+  $('.donate-container').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox')) return;
+
+      var alt = this.alt;
+
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
